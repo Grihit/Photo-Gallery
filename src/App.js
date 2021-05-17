af08641701571c2155cd785f628d0341e74dc420
Grihit <grihitbudhiraja12@gmail.com>
2021-05-17 17:37:52 +0530

    Second Draft

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import React from 'react';
 import './App.scss';
 import "./styles.css";
 import { Image } from './components/Image';
-import { popup } from './components/Popup';
 import images from './data';
 
 function App() {
   return (
     <React.Fragment>
-      <p className="heading">Photoshop Edits</p><hr/>
+      <p className="heading">Photoshop Edits</p>
       <div className="flex-container">
-          {images.map(data => (
+          {images.sort(({id: previousID}, { id: currentID })=> previousID - currentID).map(data => (
             <div className="flex-item" key={`${data.id}`}>
               <Image data={data} />
             </div>
--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import "../styles.css";
 export function Image({data}){
+    var check = false;
+    function popup(){
+        check = !check;
+      }
+    function show(){
+        
+    }
     return(
         <React.Fragment>
-            <img src={data.image} alt={data.name} width='100%' height="100%"/><span>{data.desc}</span>
+            <img src={data.image} alt={data.name} width='100%' height="100%" onClick={()=> popup()}/><span>{data.desc}</span>
+            {show()}
         </React.Fragment>
         
     )
