ef49d89ce5dba4640ca563e4a702aba425e85d9d
Grihit <grihitbudhiraja12@gmail.com>
2021-05-17 19:34:23 +0530

    Final commit

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
+import Popup from 'reactjs-popup';
 import "../styles.css";
 export function Image({data}){
-    var check = false;
-    function popup(){
-        check = !check;
-      }
-    function show(){
-        
-    }
     return(
         <React.Fragment>
-            <img src={data.image} alt={data.name} width='100%' height="100%" onClick={()=> popup()}/><span>{data.desc}</span>
-            {show()}
+        <Popup
+            trigger = {
+                <img src={data.image} alt={data.name} width='100%' height="100%" />
+            }
+            on="click"
+            modal
+            closeOnDocumentClick
+        >
+            <div className="popup">
+                <img src={data.image} alt={data.name}/>
+                <span className="name">{data.name}</span>
+            </div>
+        </Popup>
+        <span className="desc">{data.desc}</span>
         </React.Fragment>
         
     )
